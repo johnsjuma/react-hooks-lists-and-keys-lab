7b697f310e33f4d80e45796ad0d8c3af6fa6ea13
jerryjohns <jerry.johns@student.moringaschool.com>
2022-07-18 15:01:39 +0300

    Done.

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,9 +4,20 @@ import ProjectItem from "./ProjectItem";
 function ProjectList({ projects }) {
   console.log(projects);
   return (
-    <div id="projects">
+    <div id="projects">proj
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{
+        projects.map((project)=>(
+        <ProjectItem
+           key={project.id}
+            name={project.name} 
+            about={project.about} 
+            project={project.name}
+            technologies={project.technologies}
+          />))
+    
+     }
+    </div>
     </div>
   );
 }
